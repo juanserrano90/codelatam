a0fd536e1b85dd2a64712a00d2350c91c4f6f4dd
Valentina Contreras Rojas <56318148+Valeign@users.noreply.github.com>
2024-11-02 10:52:36 -0300

    Add files via upload

--- DataPreparation/modules/shift_spectrum.py
+++ DataPreparation/modules/shift_spectrum.py
@@ -2,9 +2,31 @@ import numpy as np
 from scipy import stats
 import logging
 
-def shift_spectrum(spectrum):
-    """Loosely simulate redshift estimation error by shifting the entire spectrum by at most 5 pixels left or right."""
-    shift_amount = stats.randint.rvs(-5, 6, size=1).item()
-    shifted_spectrum = np.roll(spectrum, shift_amount)
-    logging.info(f'------Shift amount: {shift_amount}')
+def shift_spectrum(spectrum, base_redshift, max_error_percentage=1):
+    """
+    Desplaza el espectro según un redshift modificado por un porcentaje aleatorio.
+    
+    Parameters:
+    - spectrum (array): El espectro de la supernova.
+    - base_redshift (float): Redshift de referencia de la supernova.
+    - max_error_percentage (float): Máximo porcentaje de error en el redshift (default: 0.1).
+    
+    Returns:
+    - array: Espectro desplazado.
+    """
+    # Genera un número aleatorio entre 0 y 1
+    random_factor = np.random.uniform(0, 1)
+    # Calcula el redshift modificado con el factor aleatorio
+    shift_redshift = base_redshift * (1 + random_factor * max_error_percentage)
+    
+    # Desplazamos el espectro usando el redshift modificado
+    shifted_spectrum = np.interp(
+        np.arange(len(spectrum)) * (1 + shift_redshift - base_redshift),
+        np.arange(len(spectrum)),
+        spectrum,
+        left=0,
+        right=0
+    )
+    
+    logging.info(f'------Redshift modificando (factor aleatorio): {shift_redshift}')
     return shifted_spectrum
